ccc5793c93433320b4ab5a0a054f130511a89c77
Collin Arbour <collinarbour@gmail.com>
2022-11-30 23:59:47 -0600

    Last attempt at processing data manually

--- scripts/make_npz.py
+++ scripts/make_npz.py
@@ -15,48 +15,57 @@ import uproot
 import awkward as ak
 from tqdm import tqdm
 
-# define the features that we want to extract
-features1 = ["fj_jetNTracks",
-            "fj_nSV",
-            "fj_eta",
-            "fj_mass",
-            "fj_phi",
-            "fj_pt",
-            "fj_ptDR",
-            "fj_relptdiff",
-            "fj_sdn2",
-            "fj_sdsj1_eta",
-            "fj_sdsj1_mass",
-            "fj_sdsj1_phi",    
-            "fj_sdsj1_pt",
-            "fj_sdsj1_ptD",
-            "fj_sdsj2_eta",
-            "fj_sdsj2_mass",
-            "fj_sdsj2_phi",
-            "fj_sdsj2_pt",
-            "fj_sdsj2_ptD",
-            "fj_z_ratio"]
-
 with uproot.open(f"../root_files/ntuple_merged_0.root:deepntuplizer/tree") as tree:
-    features2 = [x for x in tree.keys() if x[:6]=='fj_tau' or x[:8]=='fj_track']
-
-features = features1 + features2
+    features = [x for x in tree.keys() if x[:5]=='track']
 
 
-def get_labels(tree,label):
+
+def reshape(awk_ar,sorter,fet_lngth, ascending=False):
     '''
-    Function to return the labels array out of a root tree.
-    This function is required because we use 2 sets of 2 lables each
-    where each set is combined with a logical "and".
-    For instance "fj_isQCD and sample_isQCD" is the final label to label a jet as
-    originating from QCD.
+    This function creates takes an awkward array of track information in an event, and formats it in a standard 2D array.
+    The tracks of an event are sorted according to the 'sorter' argument in descending order (greatest value first).
+    Events that have too many tracks are truncated, while those with too few are padded with zeros afterwards.
+    The returned array is an array of 2D arrays containing  track information for each event.
     '''
-    prods = label.split('*')
-    facts = tree.arrays(prods,library='np')
-    labels = np.multiply(facts[prods[0]],facts[prods[1]])
-    return labels
+    evt_ar = [[] for i in range(len(awk_ar[0]))]
+    srt_idx = np.where(np.array(features)==sorter)[0][0]
+    
+    for fet in awk_ar:
+        for evt_n in range(len(fet)):
+            evt_ar[evt_n].append(np.array(fet[evt_n]))
+            
+    # Making 2D arrays for each event, where each each row of that event's 2D array is now a feature array
+    evt_ar2d = []
+    
+    for evt in evt_ar:
+        evt_ar2d.append(np.stack(evt))
+        
+    # Now sorting each 2d array's columns according to the 'sorter' feature
+    evt_ar2d_srtd = []
+    
+    for evt in evt_ar2d:
+        idcs = evt[srt_idx].argsort()[::-1]
+        evt_ar2d_srtd.append(evt[:,idcs])
+        
+    # Now need to standardize feature array per event
+    evt_std = []
+    npad = 0
+    
+    for evt in evt_ar2d_srtd:
+        if len(evt[0]) > fet_lngth:
+            evt_std.append( np.swapaxes(evt[:,:fet_lngth],0,1) )
+#             evt_std.append( evt[:,:fet_lngth].flatten() )
+        else:
+            npad += 1
+            padded = np.pad(evt,((0,0),(0,fet_lngth - len(evt[0]))))
+            evt_std.append( np.swapaxes(padded,0,1) )
+#             evt_std.append( np.pad(evt,((0,0),(0,fet_lngth - len(evt[0])))).flatten() )
+            
+    print('Number of events padded: {}'.format(npad))
+    
+    return evt_std
 
-def get_features(file_name):
+def get_features(file_name, var_sort, ntrx):
     '''
     Function that extracts our chosen feature and label arrays from a root file
     for the events that are labeled as QCD or Hbb and returns two 2D arrays.
@@ -65,11 +74,25 @@ def get_features(file_name):
     '''
     with uproot.open(f"{file_name}:deepntuplizer/tree") as tree:
 
-        feature_array = np.stack(list(tree.arrays(features,library='np').values()),axis=-1)
-        label_array = np.stack([get_labels(tree,i) for i in labels],axis=-1)
-        feature_array = feature_array[np.sum(label_array,axis=1)==1]
-        label_array = label_array[np.sum(label_array,axis=1)==1]
+        fet_data = []
+        lbl_data = []
+        
+        for fet in features:
+            fet_data.append(np.array(tree[fet]))
+            
+        for lbl in labels:
+            lbl_data.append(np.array(tree[lbl]))
 
+        feature_array = reshape(fet_data, var_sort, ntrx)
+
+        # This part organizes it by event, rather than feature. (features move from rows to columns)
+        label_array = np.stack(lbl_data,axis=-1)
+        
+        #feature_array = np.array(feature_array)[label_array.any(1)]
+        feature_array = np.array(feature_array)
+        label_array = label_array
+        
+    #     return label_array
     return feature_array, label_array
 
 
@@ -78,6 +101,8 @@ def main():
     start = int(sys.argv[1])
     end = int(sys.argv[2]) + 1
     outfile = sys.argv[3]
+    srtr = sys.argv[4]
+    ntrx = in(sys.argv[5]
 
 
     final_features = np.empty((0,nfeatures))
@@ -86,22 +111,22 @@ def main():
     for i in tqdm(range(start, end)):
         file = f"ntuple_merged_{i}.root"
         try:
-            feature_array, label_array = get_features(f'../root_files/{file}')
-            final_features = np.vstack((final_features, feature_array))
-            final_labels = np.vstack((final_labels, label_array))    
+            feature_array, label_array = get_features(f'../root_files/{file}',srtr,ntrx)
+            #final_features = np.vstack((final_features, feature_array))
+            #final_labels = np.vstack((final_labels, label_array))    
         except FileNotFoundError:
             pass
         
-    print(final_features.shape)
-    print(final_labels.shape)
+    print(features_array.shape)
+    print(label_array.shape)
 
-    np.savez(f'../root_files/{outfile}.npz', features = final_features, labels = final_labels, names = features)
+    np.savez(f'../root_files/{outfile}.npz', features = features_array, labels = label_array, names = features)
 
 if __name__ == '__main__':
 
     # 2 labels: QCD or Hbb. Logical "and" of labels is used.
-    labels = ['fj_isQCD*sample_isQCD',
-              'fj_isH*fj_isBB']
+    labels = ['sample_isQCD',
+              'label_H_bb']
 
     nfeatures = len(features)
     nlabels = len(labels)
